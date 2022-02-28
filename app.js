5a6059c99d66ba58c05c32eda400c8fc0f317bd7
Jonathan Picazo <jonathanpicazo@outlook.com>
2022-02-27 19:49:23 -0800

    ready to deploy

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 import express from 'express'
-import emailjs from '@emailjs/browser'
 import path from 'path'
 import dotenv from 'dotenv'
 import cors from 'cors'
-//import XMLHttpRequest from 'xhr2'
+import fetch from 'node-fetch'
 
+const sendMessage = async(data) => {
+  console.log(data)
+  try {
+    const response = fetch('https://api.emailjs.com/api/v1.0/email/send', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    return response
+  }
+  catch(error) {
+    console.log(error)
+  }
+}
 
+// init
 const app = express()
-//var xhr = new XMLHttpRequest()
 app.use(cors())
 app.use(express.json())
 dotenv.config()
-emailjs.init(process.env.EMAILJS_USER_ID);
 
 let port = process.env.PORT || 3000
 
@@ -21,23 +35,29 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/sendForm', (req, res) => {
+app.post('/sendForm', async (req, res) => {
   let payload = req.body
-  let parsedMessage = payload['subject'] + '\n' + payload['message'] + '\n' + payload ['email']
   let templateParams = {
     to_name: 'Jonathan Picazo',
     from_name: payload['name'],
-    message: parsedMessage
+    from_email: payload['email'],
+    from_subject: payload['subject'],
+    message: payload['message']
   }
-  emailjs.send(process.env.EMAILJS_SERVICE_ID, process.env.EMAILJS_TEMPLATE_ID, templateParams)
-  .then(function(response) {
-     console.log('SUCCESS!', response.status, response.text);
-  }, function(error) {
-     console.log('FAILED...', error);
-  });
-  res.send(templateParams)
+  let data = {
+    service_id: process.env.EMAILJS_SERVICE_ID,
+    template_id: process.env.EMAILJS_TEMPLATE_ID,
+    user_id: process.env.EMAILJS_USER_ID,
+    accessToken: process.env.EMAILJS_ACCESS_TOKEN,
+    template_params: templateParams
+  }
+
+  let r = await sendMessage(data)
+  console.log(r)
+  res.send(data)
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app is listening on port http://localhost:${port}`)
+  console.log(`EmailJS app is listening on port http://localhost:${port}`)
 })
